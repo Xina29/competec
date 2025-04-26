d726e31456717dba9b3bfe1b6e29b642c1201b0c
Xina <nicsoares723@gmail.com>
2025-04-26 11:23:15 -0300

    Update Main.java

--- aula-0/Main.java
+++ aula-0/Main.java
@@ -1,7 +1,56 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+	    
+	    Scanner entrada = new Scanner (System.in);
+	    
+	   //Ativadade 1
+	    
+		// char  nome = 'N';
+		 int  idade = 15;
+		 boolean cafe =  false;
+		
+		// Atividade 2
+		
+		//  int laranja = 19;
+	//	int uva = 13;
+	//	int soma = uva + laranja; 
+		
+		//Atividade 3
+		
+		//System.out.println("A soma entre Laranja e Uva deu " + soma);
+		
+		
+		//Atividade 4
+		
+		System.out.println("Digite o valor da Laranja:" );
+		int laranja = entrada.nextInt();
+		
+		System.out.println("Digite o valor da Uva:" );
+		int uva = entrada.nextInt();
+		
+		
+		System.out.println("A soma entre Laranja e Uva deu " + (laranja + uva) );
+		
+		
+		//Precisa ser Finalizado
+		System.out.println("Digite a inicial do seu Nome:" );
+		char nome = entrada.nextChar();
+		
+		System.out.println("A sua Inicial é " );
+		
+		
+		
+		
+		
+		
 	}
 }
